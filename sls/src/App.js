f1c22697cd7ac6b933cb45f6ff1731ed1709e7ff
connorshea15 <connorshea15@gmail.com>
2020-12-23 13:27:06 -0700

    working social media links

--- sls/src/App.js
+++ sls/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header";
 import Content from "./components/Content";
+import Socials from "./components/Socials";
 import { Affix } from 'antd';
 
 import './App.css';
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
       <Header />
       <Content />
+      <Socials />
     </div>
   );
 }
---
+++ sls/src/components/Socials/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import spotifyIcon from "../../assets/Spotify_Icon.png";
+import instagramIcon from "../../assets/Instagram_Icon.png";
+import facebookIcon from "../../assets/Facebook_Icon.png";
+
+function Socials() {
+
+    return (
+        <div>
+            <a href="https://open.spotify.com/artist/0zBEmDtb9oACxyvTjKsAOV" target="blank">
+                <img className="logo mx-5" src={spotifyIcon} alt="spotify logo" key="spotify-logo" />
+            </a>
+            <a href="https://www.instagram.com/somethinglikeseduction/" target="blank">
+                <img className="logo mx-5" src={instagramIcon} alt="instagram logo" key="instagram-logo" />
+            </a>
+            <a href="https://www.facebook.com/slseduction" target="blank">
+                <img className="logo mx-5" src={facebookIcon} alt="facebook logo" key="facebook-logo" />
+            </a>
+        </div>
+    );
+}
+
+export default Socials;
